cb908b69a27b24034d421f2d7a51729af1d2f01b
Iris153 <71736194+Iris153@users.noreply.github.com>
2021-01-22 18:07:38 -0600

    Update main.java

--- main.java
+++ main.java
@@ -4,15 +4,15 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
-import static u5reg.LeerTxt.LeerTxt;
+
 
 public class U5Reg {
 
     public static void main(String[] args) {
         
-       int[] lista = LeerTxt();
+       int[] arreglo = LeerTxt();
 
-        Scanner t = new Scanner(System.in);
+        Scanner leer = new Scanner(System.in);
         int opc = 0;
 
         while (opc != 5) {
@@ -23,37 +23,37 @@ public class U5Reg {
             System.out.println("[4] ORDENAMIENTO METODO RADIX");
             System.out.println("[5] SALIR");
             System.err.print("Ingrese una opcion > ");
-            opc = t.nextInt();
+            opc = leer.nextInt();
 
             switch (opc) {
                 case 1:
-                    Procesos.burbuja(lista);
+                    Procesos.burbuja(arreglo);
                     break;
                 case 2:
                     System.out.println("***** ARREGLO NO ORDENADO *****");
                     System.out.print("[");
-                    for (int k = 0; k < lista.length; k++) {
-                        System.out.print(lista[k] + ", ");
+                    for (int k = 0; k < arreglo.length; k++) {
+                        System.out.print(arreglo[k] + ", ");
                     }
                     System.out.println("]");
 
-                    Procesos.quicksort(lista, 0, lista.length - 1);
+                    Procesos.quicksort(arreglo, 0, arreglo.length - 1);
 
                     System.out.println("***** ARREGLO ORDENADO POR QUIKSORT *****");
                     System.out.print("[");
-                    for (int k = 0; k < lista.length; k++) {
-                        System.out.print(lista[k] + ", ");
+                    for (int k = 0; k < arreglo.length; k++) {
+                        System.out.print(arreglo[k] + ", ");
                     }
                     System.out.println("]\n");
                     break;
                 case 3:
-                    Procesos.shell(lista);
+                    Procesos.shell(arreglo);
                     break;
                 case 4:
-                    Procesos.Radix(lista);
+                    Procesos.Radix(arreglo);
                     break;
                 case 5:
-                    System.out.println("BYE! BYE!");
+                    System.out.println("USTED HA SALIDO");
                     System.exit(0);
                     break;
                 default:
@@ -70,7 +70,7 @@ public class U5Reg {
         FileReader Fr = null;
         BufferedReader br = null;
 
-        int matriz[] = null;
+        int arreglo[] = null;
 
         try {
             archivo = new File("C:\\Users\\irisv\\Desktop\\Regula5.txt"); // Ruta desde donde se lee el txt
@@ -82,18 +82,19 @@ public class U5Reg {
             while (((linea = br.readLine()) != null)) {
 
                 String a[] = linea.split(delimiter);
-                matriz = new int[a.length];
+                arreglo = new int[a.length];
 
                 for (int l = 0; l < a.length; l++) {
-                    matriz[l] = Integer.parseInt(a[l]);
+                    arreglo[l] = Integer.parseInt(a[l]);
                 }
             }
 
         } catch (IOException e) {
             System.out.println(e);
         }
-        return matriz;
+        return arreglo;
     }
 
     
 }
+
